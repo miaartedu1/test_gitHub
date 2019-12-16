a3aacf0836fbb9fa6f0414195c2b2388dbeb5220
Новый пользователь <anotheruser@mail.com>
2019-12-16 05:37:50 +0300

    #TDP-9 Поле DateOfBirth -> NO NULL

--- tdp/Tables/Persons.sql
+++ tdp/Tables/Persons.sql
@@ -4,7 +4,7 @@
     [Surname] NVARCHAR(50) NOT NULL, 
     [Name] NVARCHAR(50) NOT NULL, 
     [Patronymic] VARCHAR(50) NULL, 
-    [DateOfBirth] DATE NULL, 
+    [DateOfBirth] DATE NOT NULL, 
     [DateInput] DATETIME NULL Constraint [DF_Persons_DateInput] Default (getdate())
 )
 
